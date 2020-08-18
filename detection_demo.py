d878ee09d77ace4b8a9aa855e0e1ba0f30fd6e34
viniciusbaltoe <viniciusbaltoe@gmail.com>
2020-08-18 10:14:07 -0300

    usbcam_path add to detection_demo.py

--- detection_demo.py
+++ detection_demo.py
@@ -25,10 +25,11 @@ if YOLO_TYPE == "yolov3":
 
 image_path   = "./IMAGES/kite.jpg"
 #video_path   = "./IMAGES/test.mp4"
+#usbcam_path  = cv2.VideoCapture(0)
 
 yolo = Create_Yolo(input_size=YOLO_INPUT_SIZE)
 load_yolo_weights(yolo, Darknet_weights) # use Darknet weights
 
 detect_image(yolo, image_path, "./IMAGES/kite_pred.jpg", input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
 #detect_video(yolo, video_path, '', input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
-#detect_realtime(yolo, '', input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255, 0, 0))
+#detect_realtime(yolo, usbcam_path, input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255, 0, 0))
