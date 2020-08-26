8262813c7f6cfa69e9bb5086db5de668811a74f9
viniciusbaltoe <viniciusbaltoe@gmail.com>
2020-08-26 09:53:21 -0300

    test3 yolo in IS

--- detection_demo.py
+++ detection_demo.py
@@ -4,10 +4,12 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 import cv2
 import numpy as np
 import tensorflow as tf
+import time 
 from yolov3.yolov4 import Create_Yolo
 from yolov3.utils import load_yolo_weights, detect_image, detect_video, detect_realtime
 from is_wire.core import Channel, Message
 from yolov3.configs import *
+from yolov3.option_is import *
 
 
 if YOLO_TYPE == "yolov4":
@@ -22,16 +24,18 @@ image_path   = "./IMAGES/carros.jpeg"
 yolo = Create_Yolo(input_size=YOLO_INPUT_SIZE)
 load_yolo_weights(yolo, Darknet_weights) # use Darknet weights
 
-img_path = detect_image(yolo, image_path, "./IMAGES/kite_pred.jpg", input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
-#img_path = detect_video(yolo, video_path, '', input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
-#img_path = detect_realtime(yolo, usbcam_path, input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255, 0, 0))
+#detect_video(yolo, video_path, '', input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
+#detect_realtime(yolo, usbcam_path, input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255, 0, 0))
 
 
 channel = Channel('amqp://10.10.2.7:30000')
 
 img_message = Message()
 
-img_message.pack(to_image(image_path))
+img_message.pack(to_image(detect_image(yolo, image_path, "./IMAGES/kite_pred.jpg", input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
+))
 
 while True:
     channel.publish(img_message, topic='Vinicius.Frame')
+    print("#")
+    time.sleep(1)
