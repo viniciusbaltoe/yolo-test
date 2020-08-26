83bc853dd5027b91b505f2d63a5d51a88c9d7a5c
viniciusbaltoe <viniciusbaltoe@gmail.com>
2020-08-26 10:32:41 -0300

    detect_camera test

--- detection_demo.py
+++ detection_demo.py
@@ -15,12 +15,12 @@ if YOLO_TYPE == "yolov3":
     Darknet_weights = YOLO_V3_TINY_WEIGHTS if TRAIN_YOLO_TINY else YOLO_V3_WEIGHTS
 
 #image_path   = "./IMAGES/carros.jpeg"
-video_path   = "./IMAGES/test.mp4"
-#usbcam_path  = cv2.VideoCapture(0)
+#video_path   = "./IMAGES/test.mp4"
+usbcam_path  = cv2.VideoCapture(0)
 
 yolo = Create_Yolo(input_size=YOLO_INPUT_SIZE)
 load_yolo_weights(yolo, Darknet_weights) # use Darknet weights
 
 #detect_image(yolo, image_path, "./IMAGES/carros.jpeg", input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
-detect_video(yolo, video_path, '', input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
-#detect_realtime(yolo, usbcam_path, input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255, 0, 0))
+#detect_video(yolo, video_path, '', input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
+detect_realtime(yolo, usbcam_path, input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255, 0, 0))
--- yolov3/utils.py
+++ yolov3/utils.py
@@ -259,13 +259,6 @@ def detect_image(YoloV3, image_path, output_path, input_size=416, show=False, CL
     image = draw_bbox(original_image, bboxes, CLASSES=CLASSES, rectangle_colors=rectangle_colors)
 
     if output_path != '': cv2.imwrite(output_path, image)
-    #if show:
-    #    # Show the image
-    #    cv2.imshow("predicted image", image)
-    #    # Load and hold the image
-    #    cv2.waitKey(0)
-    #    # To close the window after the required kill value was provided
-    #    cv2.destroyAllWindows()
 
     channel = Channel('amqp://10.10.2.7:30000')
     img_message = Message()
@@ -329,18 +322,9 @@ def detect_video(YoloV3, video_path, output_path, input_size=416, show=False, CL
 
         img_message.pack(to_image(image))
     
-        while True:
-            channel.publish(img_message, topic='Vinicius.Frame')
-            print("#")
-            #time.sleep(1)
-        
-        #if show:
-        #    cv2.imshow('output', image)
-        #    if cv2.waitKey(25) & 0xFF == ord("q"):
-        #        cv2.destroyAllWindows()
-        #        break
+        channel.publish(img_message, topic='Vinicius.Frame')
+        #time.sleep(1)
 
-    #cv2.destroyAllWindows()
 
 # detect from webcam
 def detect_realtime(YoloV3, output_path, input_size=416, show=False, CLASSES=YOLO_COCO_CLASSES, score_threshold=0.3, iou_threshold=0.45, rectangle_colors=''):
@@ -353,6 +337,10 @@ def detect_realtime(YoloV3, output_path, input_size=416, show=False, CLASSES=YOL
     fps = int(vid.get(cv2.CAP_PROP_FPS))
     codec = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter(output_path, codec, fps, (width, height)) # output_path must be .mp4
+
+    # IS config to "Publisher"
+    channel = Channel('amqp://10.10.2.7:30000')
+    img_message = Message()
 
     while True:
         _, frame = vid.read()
@@ -387,11 +375,7 @@ def detect_realtime(YoloV3, output_path, input_size=416, show=False, CLASSES=YOL
         image = cv2.putText(frame, "Time: {:.1f}FPS".format(fps), (0, 30),
                           cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
 
-        if output_path != '': out.write(frame)
-        if show:
-            cv2.imshow('output', frame)
-            if cv2.waitKey(25) & 0xFF == ord("q"):
-                cv2.destroyAllWindows()
-                break
-
-    cv2.destroyAllWindows()
+        img_message.pack(to_image(frame))
+    
+        channel.publish(img_message, topic='Vinicius.Frame')
+        #time.sleep(1)
