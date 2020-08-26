d7d76e9b7c1af32d040c5dc12128ef871aa66bef
viniciusbaltoe <viniciusbaltoe@gmail.com>
2020-08-26 10:07:59 -0300

    detect_img working

--- detection_demo.py
+++ detection_demo.py
@@ -9,7 +9,7 @@ from yolov3.yolov4 import Create_Yolo
 from yolov3.utils import load_yolo_weights, detect_image, detect_video, detect_realtime
 from is_wire.core import Channel, Message
 from yolov3.configs import *
-from yolov3.option_is import *
+from yolov3.options_is import *
 
 
 if YOLO_TYPE == "yolov4":
@@ -17,24 +17,26 @@ if YOLO_TYPE == "yolov4":
 if YOLO_TYPE == "yolov3":
     Darknet_weights = YOLO_V3_TINY_WEIGHTS if TRAIN_YOLO_TINY else YOLO_V3_WEIGHTS
 
-image_path   = "./IMAGES/carros.jpeg"
+#image_path   = "./IMAGES/carros.jpeg"
 #video_path   = "./IMAGES/test.mp4"
 #usbcam_path  = cv2.VideoCapture(0)
 
 yolo = Create_Yolo(input_size=YOLO_INPUT_SIZE)
 load_yolo_weights(yolo, Darknet_weights) # use Darknet weights
 
-#detect_video(yolo, video_path, '', input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
-#detect_realtime(yolo, usbcam_path, input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255, 0, 0))
+#img = detect_image(yolo, image_path, "./IMAGES/carros.jpeg", input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
+#img = detect_video(yolo, video_path, '', input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
+#img = detect_realtime(yolo, usbcam_path, input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255, 0, 0))
 
 
 channel = Channel('amqp://10.10.2.7:30000')
 
 img_message = Message()
+#detect_image
+img_message.pack(to_image(img))
+#detect_video
 
-img_message.pack(to_image(detect_image(yolo, image_path, "./IMAGES/kite_pred.jpg", input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
-))
-
+#detect_realtime
 while True:
     channel.publish(img_message, topic='Vinicius.Frame')
     print("#")
